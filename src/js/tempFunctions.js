8a08c04da0ad1f603d8ba7e2fff10df6f05d9f47
Callan2256 <callan.m@windowslive.com>
2021-09-29 13:49:04 +1300

    JS can now take user input
    Login form accepts and can handle user input

--- src/js/tempFunctions.js
+++ src/js/tempFunctions.js
@@ -1,7 +1,19 @@
+//Login Form
 const loginPanel = document.getElementById('loginContainer');
 const loginBtn = document.getElementById('loginBtn');
+const loginForm = document.getElementById('loginForm');
+const loginSubmit = document.getElementById('submitButton');
+
 
 loginBtn.addEventListener('click', event => {
     console.log("test");
     loginPanel.classList.remove('hidden');
-})
+});
+
+loginSubmit.addEventListener('click', event => {
+    event.preventDefault();
+    let username = loginForm.username.value;
+    let password = loginForm.password.value;
+
+    console.log(username, password);
+});
