8155a14b80f50ae2a4b437b6d25553d16fc31b17
Callan2256 <callan.m@windowslive.com>
2021-09-29 14:41:08 +1300

    Differentiated Login / Signup
    Login / Signup buttons now display different variations of the panel.
    
    Login includes a "Login Header" and a submit button
    
    Create Account includes a "Create Account" header and a sign up button

--- src/js/tempFunctions.js
+++ src/js/tempFunctions.js
@@ -1,19 +1,37 @@
-//Login Form
+//Login
 const loginPanel = document.getElementById('loginContainer');
 const loginBtn = document.getElementById('loginBtn');
 const loginForm = document.getElementById('loginForm');
-const loginSubmit = document.getElementById('submitButton');
+const loginSubmit = document.getElementById('loginSubmitButton');
+const loginHeader = document.getElementById('loginHeader');
+
+//Sign up
+const signupBtn = document.getElementById('signupBtn');
+const signupSubmit = document.getElementById('signupSubmitButton');
+const signupForm = document.getElementById('signupForm');
+const signupHeader = document.getElementById('signupHeader');
 
 
 loginBtn.addEventListener('click', event => {
-    console.log("test");
+    console.log("Login");
     loginPanel.classList.remove('hidden');
+    loginForm.classList.remove('hidden');
+    loginHeader.classList.remove('hidden');
 });
+
+signupBtn.addEventListener('click', event => {
+    console.log("Signup");
+    loginPanel.classList.remove('hidden');
+    signupForm.classList.remove('hidden');
+    signupHeader.classList.remove('hidden');
+});
+
+
 
 loginSubmit.addEventListener('click', event => {
     event.preventDefault();
-    let username = loginForm.username.value;
-    let password = loginForm.password.value;
+    let username = signupForm.username.value;
+    let password = signupForm.password.value;
 
     console.log(username, password);
 });
