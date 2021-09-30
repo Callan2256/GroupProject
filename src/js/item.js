1915869c84226e88b342f5a7ab8597b57526fbb0
Callan2256 <callan.m@windowslive.com>
2021-10-01 11:32:52 +1300

    Added items class

---
+++ src/js/item.js
@@ -0,0 +1,9 @@
+class item {
+    constructor (id, name, price, desc, imgPath) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.desc = desc;
+        this.imgPath = imgPath;
+    }
+}
--- src/js/model.js
+++ src/js/model.js
@@ -2,7 +2,11 @@
 
 const model = {
     users: [],
+    items: [],
     add(user) {
         this.users.push(user);
+    },
+    addItem(item) {
+        this.items.push(item);
     }
 }
