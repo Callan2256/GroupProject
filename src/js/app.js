62dbb0bbf5f6168be71e897f9cee5e73eb2bc4ba
Raine <legaspi.raine@yahoo.com>
2021-10-17 22:01:27 +1300

    routes

--- src/js/app.js
+++ src/js/app.js
@@ -5,21 +5,25 @@ const http = require("http");
 
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const bodyParser = require("body-parser"); // middleware
 
+const mongoose = require("mongoose");
+
 //ROUTES
-
-const path = require("path");
-
 // Route to Homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "html", "index.html"));
 });
+app.use(express.static(path.join(__dirname, "..", "css")));
 
 app.get("/login", (req, res) => {
   //res.sendFile(path.join(__dirname, "..", "html", "index.html"));
 });
+
+//connect to database
+// mongoose.connect("");
 
 //create server
 const server = http.createServer(app).listen(3000);
