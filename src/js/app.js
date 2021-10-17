5ed3347564afd60668904052315eff966dcfb16f
Callan2256 <callan.m@windowslive.com>
2021-10-18 12:11:57 +1300

    Restructured for MVC
    Restructured file structure for MVC (Model, View, Controller).

--- src/js/app.js
+++ src/js/app.js
@@ -11,20 +11,20 @@ const path = require("path");
 
 //connect to database
 mongoose
-  .connect(process.env.DB_CONNECTION)
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.DB_CONNECTION)
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 //ROUTES
 //Route to Homepage
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "html", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "view", "html", "index.html"));
 });
-app.use(express.static(path.join(__dirname, "..", "css")));
+app.use(express.static(path.join(__dirname, "..", "view", "css")));
 
 //starting server
 app.listen(3000, console.log(`listening on port ${PORT}`));
