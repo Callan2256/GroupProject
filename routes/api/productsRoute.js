e4b43fd2d6a36a55410950a9b4eef0a686532a29
Raine <legaspi.raine@yahoo.com>
2021-10-18 18:55:52 +1300

    add products to db

--- routes/api/productsRoute.js
+++ routes/api/productsRoute.js
@@ -1,13 +1,24 @@
+const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 
 //product Model
-const Products = require("../../src/model/Products");
+const Products = require("../../src/model/ProductSchema");
 
 //@routes POST api/products
 //@desc Create a product
-router.post("/", (req, res) => {
-  res.send("Create a product");
+router.post("/", async (req, res) => {
+  const newProduct = new Products(req.body);
+
+  try {
+    const product = await newProduct.save(); //save new product to the db
+    if (!product) {
+      throw Error("Something went wrong while saving new product");
+    }
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(400).send({ msg: err });
+  }
 });
 
 module.exports = router;
--- src/js/app.js
+++ src/js/app.js
@@ -11,32 +11,40 @@ const path = require("path");
 
 //connect to database
 mongoose
-    .connect(process.env.DB_CONNECTION)
-    .then(() => {
-        console.log("MongoDB connected");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  .connect(process.env.DB_CONNECTION)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//bodyparser middleware
+app.use(express.json());
 
 //ROUTES
+
+const productRoutes = require("../../routes/api/productsRoute");
+//create product
+app.use("/api/products", productRoutes);
+
 //Route to Homepage
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "view", "html", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "view", "html", "index.html"));
 });
 
 app.use(express.static(path.join(__dirname, "..", "/public")));
 
 app.get("/model/user.js", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "model", "user.js"));
+  res.sendFile(path.join(__dirname, "..", "model", "user.js"));
 });
 
 app.get("/model/products.js", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "model", "products.js"));
+  res.sendFile(path.join(__dirname, "..", "model", "products.js"));
 });
 
 app.get("/controller/controller.js", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "controller", "controller.js"));
+  res.sendFile(path.join(__dirname, "..", "controller", "controller.js"));
 });
 
 //starting server
