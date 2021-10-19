7fc34dc457d5c6f77c89c0a0d47fb5889d401af6
Callan2256 <callan.m@windowslive.com>
2021-10-19 20:01:57 +1300

    Encrypted user login functionality
    Users can now login with encrypted passwords, Username is displayed

--- routes/api/productsRoute.js
+++ routes/api/productsRoute.js
@@ -7,33 +7,33 @@ const Products = require("../../src/model/ProductSchema");
 
 //@routes GET api/products
 //@desc get products
-router.get("/", async (req, res) => {
-  try {
-    const product = await Products.find();
-    if (!product) {
-      throw Error("No items");
+router.get("/", async(req, res) => {
+    try {
+        const product = await Products.find();
+        if (!product) {
+            throw Error("No items");
+        }
+        res.status(200).json(product);
+        //console.log(product);
+    } catch (err) {
+        res.status(400).send({ msg: err });
     }
-    res.status(200).json(product);
-    console.log(product);
-  } catch (err) {
-    res.status(400).send({ msg: err });
-  }
 });
 
 //@routes POST api/products
 //@desc Create a product
-router.post("/", async (req, res) => {
-  const newProduct = new Products(req.body);
-  console.log(req.body.name);
-  try {
-    const product = await newProduct.save(); //save new product to the db
-    if (!product) {
-      throw Error("Something went wrong while saving new product");
+router.post("/", async(req, res) => {
+    const newProduct = new Products(req.body);
+    console.log(req.body.name);
+    try {
+        const product = await newProduct.save(); //save new product to the db
+        if (!product) {
+            throw Error("Something went wrong while saving new product");
+        }
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(400).send({ msg: err });
     }
-    res.status(200).json(product);
-  } catch (err) {
-    res.status(400).send({ msg: err });
-  }
 });
 
 module.exports = router;
--- routes/api/usersRoute.js
+++ routes/api/usersRoute.js
@@ -16,115 +16,98 @@ let users = new Array();
 
 //@routes GET api/users
 //@desc get users
-router.get("/", async (req, res) => {
-  try {
-    const user = await Users.find();
-    if (!user) {
-      throw Error("No users");
+router.get("/", async(req, res) => {
+    try {
+        const user = await Users.find();
+        if (!user) {
+            throw Error("No users");
+        }
+        res.status(200).json(user);
+        console.log(user);
+    } catch (err) {
+        res.status(400).send({ msg: err });
     }
-    res.status(200).json(user);
-    console.log(user);
-  } catch (err) {
-    res.status(400).send({ msg: err });
-  }
 });
 
 //Create account
-router.post("/", async (req, res) => {
-  console.log("req.body: " + JSON.stringify(req.body));
+router.post("/", async(req, res) => {
+    console.log("req.body: " + JSON.stringify(req.body));
 
-  const newUser = new Users({
-    //id: req.body.id,
-    name: req.body.username,
-    password: await encryptPass(req.body.password),
-    isAdmin: true,
-  });
-  console.log(newUser);
+    const newUser = new Users({
+        //id: req.body.id,
+        name: req.body.username,
+        password: await encryptPass(req.body.password),
+        isAdmin: true,
+    });
+    console.log(newUser);
 
-  try {
-    const user = await newUser.save(); //save new user to the db
-    if (!user) {
-      throw Error("Something went wrong while saving new user");
+    try {
+        const user = await newUser.save(); //save new user to the db
+        if (!user) {
+            throw Error("Something went wrong while saving new user");
+        }
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(400).send({ msg: err });
     }
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(400).send({ msg: err });
-  }
 });
 
 //login
-router.post("/login", async (req, res) => {
-  console.log("login");
-  console.log("req.body: " + req.body);
+router.post("/login", async(req, res) => {
+    //console.log("login");
+    //console.log("req.body: " + req.body);
 
-  try {
-    const user = await Users.findOne({
-      name: req.body.username,
-    });
-    if (!user) {
-      throw Error("user not found");
+    try {
+        const user = await Users.findOne({
+            name: req.body.username,
+        });
+        if (!user) {
+            throw Error("user not found");
+        }
+
+        const match = await bcrypt.compare(req.body.password, user.password);
+
+        if (match) {
+
+            let username = user.name;
+            let admin = user.isAdmin;
+
+            let response = {
+                "Result": "Success",
+                "username": username,
+                "admin": admin
+            }
+
+            res.status(200).json(response);
+            return;
+        } else {
+            res.status(400).send(
+                JSON.stringify({
+                    "Result": "Unsuccessful"
+                })
+            );
+        }
+
+        //console.log(user);
+    } catch (err) {
+        console.log(err)
     }
-    res.status(200).json(user);
-    const match = await bcrypt.compare(req.body.password, user.password);
-    if (match) {
-      res.status(200).send(
-        JSON.stringify({
-          Result: "Success",
-          // id: user.id,
-          username: user.username,
-          admin: user.admin,
-        })
-      );
-
-      return;
-    } else {
-      res.status(400).send(
-        JSON.stringify({
-          Result: "Unsuccessful",
-        })
-      );
-    }
-
-    console.log(user);
-  } catch (err) {
-    res.status(400).send({ msg: err });
-  }
-
-  //compare passwords when logging in
-  const match = await bcrypt.compare(password, user.password);
-  if (match) {
-    res.status(200).send(
-      JSON.stringify({
-        Result: "Success",
-        id: user.id,
-        username: user.username,
-        admin: user.admin,
-      })
-    );
-    return;
-  } else {
-    res.status(400).send(
-      JSON.stringify({
-        Result: "Unsuccessful",
-      })
-    );
-  }
 });
 
 //General Functions
 
 async function encryptPass(password) {
-  let salt = "";
-  let hash = "";
+    let salt = "";
+    let hash = "";
 
-  try {
-    salt = await bcrypt.genSalt(saltRounds);
-    hash = await bcrypt.hash(password, salt);
-  } catch {
-    console.log("Error Encrypting Pass");
-  }
+    try {
+        salt = await bcrypt.genSalt(saltRounds);
+        hash = await bcrypt.hash(password, salt);
+    } catch {
+        console.log("Error Encrypting Pass");
+    }
 
-  return hash;
+    return hash;
 }
 
 // async function adminUsers() {
