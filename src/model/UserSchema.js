70499fc7825420ba916d9729114a851905981b6d
Raine <legaspi.raine@yahoo.com>
2021-10-19 14:54:08 +1300

    ecnrypted pass working

--- src/model/UserSchema.js
+++ src/model/UserSchema.js
@@ -8,33 +8,8 @@ const saltRounds = 10;
 
 const UserSchema = new Schema({
   name: { type: String, require: true },
-  password: { type: Number, require: true },
+  password: { type: String, require: true },
   isAdmin: { type: Boolean },
 });
-
-// UserSchema.pre("save", function (next) {
-//   console.log("encryption ....");
-//   const user = this;
-
-//   if (this.isModified("password") || this.isNew) {
-//     //generate salt
-//     bcrypt.genSalt(10, function (saltError, salt) {
-//       if (saltError) {
-//         return next(saltError);
-//       } else {
-//         //hash password
-//         bcrypt.hash(user.password, salt, function (hashError, hash) {
-//           if (hashError) {
-//             return next(hashError);
-//           }
-//           user.password = hash;
-//           next();
-//         });
-//       }
-//     });
-//   } else {
-//     return next();
-//   }
-// });
 
 module.exports = mongoose.model("Users", UserSchema);
